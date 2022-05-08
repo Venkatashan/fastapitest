ad05bf4e54fbb3db3c0d320f58bbbd7d1a45a854
venkatatashan <futurecreaterr@outlook.com>
2022-05-08 11:13:58 +0530

    added model.py

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-from fastapi import FastAPI
+#Twitter: Bek Brace
+#Instagram: Bek Brace
+#uvicorn main:app --reload
+#python -m uvicorn main:app --reload
+
+import uvicorn
+from fastapi import FastAPI, Body, Depends
+
+from model import PostSchema, UserSchema, UserLoginSchema
+from auth_bearer import JWTBearer
+from auth_handler import signJWT
 
 app = FastAPI()
 
+posts = [
+    {
+        "id": 1,
+        "title": "Penguins ",
+        "text": "Penguins are a group of aquatic flightless birds."
+    },
+    {
+        "id": 2,
+        "title": "Tigers ",
+        "text": "Tigers are the largest living cat species and a memeber of the genus panthera."
+    },
+    {
+        "id": 3,
+        "title": "Koalas ",
+        "text": "Koala is arboreal herbivorous maruspial native to Australia."
+    },
+]
+
+users = []
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+# Get Posts
+@app.get("/user")
+def get_posts():
+    return { "data": posts }
