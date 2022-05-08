0652660662fce410a6468fbaa283952074645239
venkatatashan <futurecreaterr@outlook.com>
2022-05-08 11:33:55 +0530

    added py files

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from model import PostSchema, UserSchema, UserLoginSchema
 from auth_bearer import JWTBearer
 from auth_handler import signJWT
 
-app = FastAPI()
 
 posts = [
     {
@@ -32,11 +31,64 @@ posts = [
 
 users = []
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = FastAPI()
+
+
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
+
+# route handlers
+
+# testing
+@app.get("/", tags=["test"])
+def greet():
+    return {"hello": "world!."}
+
 
 # Get Posts
-@app.get("/user")
+@app.get("/posts", dependencies=[Depends(JWTBearer())],tags=["posts"])
 def get_posts():
     return { "data": posts }
+
+
+@app.get("/posts/{id}", dependencies=[Depends(JWTBearer())], tags=["posts"])
+def get_single_post(id: int):
+    if id > len(posts):
+        return {
+            "error": "No such post with the supplied ID."
+        }
+
+    for post in posts:
+        if post["id"] == id:
+            return {
+                "data": post
+            }
+
+
+@app.post("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
+def add_post(post: PostSchema):
+    post.id = len(posts) + 1
+    posts.append(post.dict())
+    return {
+        "data": "post added."
+    }
+
+
+@app.post("/user/signup", tags=["user"])
+def create_user(user: UserSchema = Body(...)):
+    users.append(user) # replace with db call, making sure to hash the password first
+    return signJWT(user.email)
+
+
+@app.post("/user/login", tags=["user"])
+def user_login(user: UserLoginSchema = Body(...)):
+    if check_user(user):
+        return signJWT(user.email)
+    return {
+        "error": "Wrong login details!"
+    }
