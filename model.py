f2f869aa1fb649cc9ba1c5c8a0342090de728030
venkatatashan <futurecreaterr@outlook.com>
2022-05-12 20:12:38 +0530

    added py files

--- model.py
+++ model.py
@@ -3,17 +3,10 @@ from uuid import UUID,uuid4
 from pydantic import BaseModel
 
 class User(BaseModel):
-    id:Optional[UUID]=uuid4()
-    businessPhones: list
-    displayName: str
-    givenName: str
-    jobTitle: str
-    mail: str
-    mobilePhone: str
-    officeLocation: str
-    preferredLanguage: str
-    surname: str
-    userPrincipalName: str
+    id:str
+    userName:str
+    externalId:str
+
 
 class UserUpdateRequest(BaseModel):
     businessPhones: list
