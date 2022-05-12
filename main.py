499c6dd19033d82a2ca304040d27a0cff63934b0
venkatatashan <futurecreaterr@outlook.com>
2022-05-12 16:27:24 +0530

    added py files

--- main.py
+++ main.py
@@ -1,117 +1,101 @@
-#Twitter: Bek Brace
-#Instagram: Bek Brace
-#uvicorn main:app --reload
-#python -m uvicorn main:app --reload
+from typing import Optional,List
+from fastapi import FastAPI,HTTPException
+#from sqlalchemy.testing.suite.test_reflection import users
 
-import uvicorn
-from fastapi import FastAPI, Body, Depends
-
-from model import UserSchema, UserLoginSchema
-from auth_bearer import JWTBearer
+from model import User,UserUpdateRequest,UserLoginSchema
+from uuid import UUID,uuid4
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import JSONResponse
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from pydantic import BaseModel
 from auth_handler import signJWT
-
-
-posts = [
-    {
-        "id":1,
-        "externalId":"userext1",
-        "meta":"TestMetta",
-        "businessPhones": "044284789",
-        "displayName": "TestScimAPI",
-        "userName": "TestSCIM1",
-        "givenName": "TestSCIMuser",
-        "jobTitle": "scimendpoint",
-        "mail": "scimendpoint@.onmicrosoft.com",
-        "mobilePhone": "1234567890",
-        "officeLocation": "ind",
-        "preferredLanguage": "eng",
-        "userPrincipalName": "scimendpoint@M365x214355.onmicrosoft.com"
-    },
-    {
-        "id": 2,
-        "externalId": "userext2",
-        "meta": "TestMetta",
-        "businessPhones": "044284789",
-        "displayName": "TestScimAPI",
-        "userName": "TestSCIM2",
-        "givenName": "TestSCIMuser",
-        "jobTitle": "scimendpoint",
-        "mail": "scimendpoint@.onmicrosoft.com",
-        "mobilePhone": "1234567890",
-        "officeLocation": "ind",
-        "preferredLanguage": "eng",
-        "userPrincipalName": "scimendpoint@M365x214355.onmicrosoft.com"
-    },
-    {
-        "id": 3,
-        "externalId": "userext3",
-        "meta": "TestMetta",
-        "businessPhones": "044284789",
-        "displayName": "TestScimAPI",
-        "userName": "TestSCIM3",
-        "givenName": "TestSCIMuser",
-        "jobTitle": "scimendpoint",
-        "mail": "scimendpoint@.onmicrosoft.com",
-        "mobilePhone": "1234567890",
-        "officeLocation": "ind",
-        "preferredLanguage": "eng",
-        "userPrincipalName": "scimendpoint@M365x214355.onmicrosoft.com"
-    },
-]
-
-users = []
 
 app = FastAPI()
 
+db: List[User]=[
+    User(id = UUID("434f46a1-bfb6-4e98-8a8d-ca7049909f7c"),
+    businessPhones=["+1 412 555 0109"],
+    displayName="Megan Bowen",
+    givenName="Megan",
+    jobTitle="Auditor",
+    mail="MeganB@M365x214355.onmicrosoft.com",
+    mobilePhone="123 456 789",
+    officeLocation="12/1110",
+    preferredLanguage="en-US",
+    surname="Bowen",
+    userPrincipalName="MeganB@M365x214355.onmicrosoft.com"),
 
+    User(id=UUID("c9807ff6-cc54-4034-9c74-df1cb2e69b06"),
+         businessPhones=["+1 412 555 01026"],
+         displayName="Venkat Krish",
+         givenName="Venkat",
+         jobTitle="Auditor",
+         mail="VenkatK@M365x214355.onmicrosoft.com",
+         mobilePhone="123 456 000",
+         officeLocation="15/1111",
+         preferredLanguage="en-US",
+         surname="Krish",
+         userPrincipalName="venkatK@M365x214355.onmicrosoft.com")
+]
 
-def check_user(data: UserLoginSchema):
-    for user in users:
-        if user.mail == data.mail and user.userName == data.userName:
+def check_user(user: UserLoginSchema):
+    for user in user:
+        if user.id == user.id and user.password == user.password:
             return True
     return False
 
+@app.post("/scim/Token")
+async def signup_user(user: UserLoginSchema):
+    db.append(user) # replace with db call, making sure to hash the password first
+    return signJWT(user.id)
 
-# route handlers
-
-# testing
-@app.get("/", tags=["test"])
-def greet():
-    return {"hello": "world!."}
-
-@app.post("/scim/token", tags=["user"])
-def create_user(user: UserLoginSchema = Body(...)):
-    users.append(user) # replace with db call, making sure to hash the password first
-    return signJWT(user.mail)
-
-
-@app.post("/scim/login", tags=["user"])
-def user_login(user: UserLoginSchema = Body(...)):
+@app.post("/scim/v2/Users/login")
+def user_login(user: UserLoginSchema):
     if check_user(user):
-        return signJWT(user.mail)
+        return signJWT(user.id)
     return {
         "error": "Wrong login details!"
     }
-
-@app.post("/scim/v2/Users", dependencies=[Depends(JWTBearer())], tags=["posts"])
-def add_post(post: UserSchema):
-    post.id = len(posts) + 1
-    posts.append(post.dict())
-    return {
-        "data": "post added."
-    }
-
-@app.get("/scim/v2/Users/", tags=["posts"])
-def get_single_post(id: int):
-    if id > len(posts):
-        return {
-            "error": "No such post with the supplied ID."
-        }
-
-    for post in posts:
-        if post["id"] == id:
-            return {
-                "data": post
-            }
+@app.get("/")
+async def read_root():
+    return {"Hello": "Venkat"}
 
 
+@app.get("/scim/v2/Users")
+async def fetch_users():
+    return db
+
+@app.post("/scim/v2/Users")
+async def create_user(user:User):
+    db.append(user)
+    return {"id":user.id}
+
+@app.delete("/scim/v2/Users/{id}")
+async def delete_user(user_id:UUID):
+    for user in db:
+        if user.id==user_id:
+            db.remove(user)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist"
+    )
+
+@app.put("scim/v2/Users/{id}")
+async def update_user(user_update:UserUpdateRequest,user_id:UUID):
+    for user in db:
+        if user.id==user_id:
+            if user_update.businessPhones is not None:
+                user.businessPhones = user_update.businessPhones
+            if user_update.mobilePhone is not None:
+                user.mobilePhone = user_update.mobilePhone
+            if user_update.officeLocation is not None:
+                user.officeLocation = user_update.officeLocation
+            if user_update.jobTitle is not None:
+                user.jobTitle = user_update.jobTitle
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist"
+    )
--- model.py
+++ model.py
@@ -1,53 +1,26 @@
-from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
+from uuid import UUID,uuid4
+from pydantic import BaseModel
 
-class UserSchema(BaseModel):
+class User(BaseModel):
+    id:Optional[UUID]=uuid4()
+    businessPhones: list
+    displayName: str
+    givenName: str
+    jobTitle: str
+    mail: str
+    mobilePhone: str
+    officeLocation: str
+    preferredLanguage: str
+    surname: str
+    userPrincipalName: str
 
-    id: int = Field(default=None)
-    externalId:str = Field(default=None)
-    meta:str= Field(default=None)
-    businessPhones : str = Field(...)
-    userName : str = Field(...)
-    displayName:str = Field(...)
-    givenName:str = Field(...)
-    jobTitle:str = Field(...)
-    mail:str = Field(...)
-    mobilePhone:str = Field(...)
-    officeLocation:str = Field(...)
-    preferredLanguage:str = Field(...)
-    surname:str = Field(...)
-    userPrincipalName:str = Field(...)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": 4,
-                "externalId": "userext",
-                "meta": "TestMetta",
-                "businessPhones": "044284789",
-                "userName": "TestSCIM",
-                "displayName": "TestScimAPI",
-                "givenName": "TestSCIMuser",
-                "jobTitle": "scimendpoint",
-                "mail": "scimendpoint@.onmicrosoft.com",
-                "mobilePhone": "1234567890",
-                "officeLocation": "ind",
-                "preferredLanguage": "eng",
-                "surname":"endpoint",
-                "userPrincipalName": "scimendpoint@M365x214355.onmicrosoft.com"
-            }
-        }
-
+class UserUpdateRequest(BaseModel):
+    businessPhones: list
+    jobTitle: str
+    mobilePhone: str
+    officeLocation: str
 
 class UserLoginSchema(BaseModel):
-
-    userName : str = Field(...)
-    mail: str = Field(...)
-
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "userName": "scimendpoint azureAD",
-                "mail": "scimendpointazuread@x.com",
-            }
-        }
+    id:str
+    password:str
